4275fa326145431289225940e81aee0136c0e210
Aliaksei <44787029+Sayres11@users.noreply.github.com>
2021-06-04 14:51:55 +0300

    Delete ProjectS22141/src/po1 directory

--- ProjectS22141/src/po1/Ciezarowka.java
+++
@@ -1,9 +0,0 @@
-package po1;
-
-public class Ciezarowka extends Samochod {
-       int ladownosc;
-    public Ciezarowka(String silnik, int drzwi,int ladownosc) {
-        super(silnik, drzwi);
-        this.ladownosc = ladownosc;
-    }
-}
