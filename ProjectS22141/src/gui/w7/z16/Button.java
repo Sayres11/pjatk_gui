350cff30129b13a68c930e132f24fcd9294ff328
Aliaksei <44787029+Sayres11@users.noreply.github.com>
2021-06-07 23:34:13 +0300

    Delete ProjectS22141/src/gui/w7/z16 directory

--- ProjectS22141/src/gui/w7/z16/Button.java
+++
@@ -1,8 +0,0 @@
-package gui.w7.z16;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class Button extends JPanel  {
-
-}
--- ProjectS22141/src/gui/w7/z16/main.java
+++
@@ -1,7 +0,0 @@
-package gui.w7.z16;
-
-public class main {
-    public static void main(String[] args) {
-
-    }
-}
